8160723a71a39b8bea4a456759c8e4b6aecc61e9
Dinovo-Ilmira <dinovo.ilmira@gmail.com>
2024-01-27 20:57:18 -0500

    new file:   .idea/.gitignore         new file:   .idea/burger.iml         new file:   .idea/modules.xml         new file:   fonts/Gilroy-Semibold.ttf         new file:   fonts/Merriweather-Black.ttf         new file:   images/1.png         new file:   images/10.png         new file:   images/11.png         new file:   images/12.png         new file:   images/2.png         new file:   images/3.png         new file:   images/4.png         new file:   images/5.png         new file:   images/6.png         new file:   images/7.png         new file:   images/8.png         new file:   images/9.png         new file:   images/Logo.png         new file:   images/burger-em.png         new file:   images/burgers_bg.png         new file:   images/b\320\276\321\205-burger.png         new file:   images/food-delivery.png         new file:   images/foodtruck.png         new file:   images/main_bg.png         new file:   images/main_burger.png         new file:   images/meat.png         new file:   images/order_bg.png         new file:   images/whybg.png         new file:   index.html         new file:   scripts/script.js         new file:   styles/style.css

---
+++ scripts/script.js
@@ -0,0 +1,76 @@
+//document.getElementsByClassName("main-title")[0].style.color = "red";
+
+document.getElementById("main-action-button").onclick = function () {
+    document.getElementById("products").scrollIntoView({behavior: "smooth"});
+}
+
+let links = document.querySelectorAll(".menu-item > a");
+for (let i= 0; i < links.length; i++) {
+    links[i].onclick = function () {
+        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior: "smooth"});
+    }
+}
+
+let buttons = document.getElementsByClassName("product-button");
+for (let i= 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById("order").scrollIntoView({behavior: "smooth"});
+    }
+}
+
+let burger = document.getElementById("burger");
+let name = document.getElementById("name");
+let phone = document.getElementById("phone");
+document.getElementById("order-action").onclick = function () {
+    let hasError = false;
+
+    [burger, name, phone].forEach(item => {
+        if (!item.value){
+            item.parentElement.style.background = "red";
+            hasError = true;
+        } else {
+            item.parentElement.style.background = "";
+
+        }
+
+    });
+
+    if (!hasError) {
+        [burger, name, phone].forEach(item => {
+            item.value = "";
+        });
+        alert("Спасибо за заказ! Мы скоро свяжемся с Вами!");
+    }
+
+}
+
+let prices = document.getElementsByClassName("products-item-price");
+
+document.getElementById("change-currency").onclick = function (e) {
+    let curretCurrency = e.target.innerText;
+
+    let newCurrency = "$";
+    let coefficient = 1;
+
+    if (curretCurrency === "$"){
+        newCurrency = "₽";
+        coefficient = 80;
+    } else if(curretCurrency === "₽"){
+        newCurrency = "BYN";
+        coefficient = 3;
+    } else if(curretCurrency === "BYN"){
+        newCurrency = "€";
+        coefficient = 2;
+    } else if(curretCurrency === "€"){
+        newCurrency = "¥";
+        coefficient = 2;
+    }
+    e.target.innerText = newCurrency;
+
+    for (let i = 0; i <prices.length; i++) {
+        prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
+
+    }
+
+
+}
